0596ff5f82e37154c8ed7ad6cdd4b556938dc6f5
Louis-Rémi Babé <lrbabe@gmail.com>
2018-06-08 09:29:21 +0200

    Update pack prices and service fees

--- const.js
+++ const.js
@@ -16,24 +16,24 @@ module.exports = {
       privilege: 149000,
     },
     lyon: {
-      basic:      59000,
-      comfort:    79000,
-      privilege:  99000,
+      basic:      19000,
+      comfort:    39000,
+      privilege:  59000,
     },
     montpellier: {
-      basic:      49000,
-      comfort:    69000,
-      privilege:  89000,
-    },
-    toulouse: {
       basic:      29000,
       comfort:    49000,
       privilege:  69000,
     },
+    toulouse: {
+      basic:      19000,
+      comfort:    39000,
+      privilege:  59000,
+    },
     lille: {
-      basic:      49000,
-      comfort:    69000,
-      privilege:  89000,
+      basic:      19000,
+      comfort:    39000,
+      privilege:  59000,
     },
   },
   DEPOSIT_PRICES: {
@@ -71,9 +71,9 @@ module.exports = {
   },
 
   SERVICE_FEES: {
-    1:              5000, // 1 room
-    2:              4000, // 2 rooms
-    default:        3000, // 3 or more rooms
+    1:              6000, // 1 room
+    2:              6000, // 2 rooms
+    default:        6000, // 3 or more rooms
   },
 
   CHECKIN_DURATION:   30,
