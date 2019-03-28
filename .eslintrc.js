352df4d7b8cd6aacac73454fc47179dcf48fdb4c
Julien Kilo <julienkilo@gmail.com>
2019-03-28 14:45:26 +0100

    remove useless devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,7 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-  },
   extends: [
     'eslint-config-airbnb-base',
   ],
-  plugins: [
-    'babel',
-    'import',
-  ],
-  settings: {
-    'import/resolver': 'babel-root-import',
-  },
-  env: {
-    'browser': true,
-    'amd': true,
-    'es6': true,
-    'node': true,
-    'mocha': true
-  },
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'indent': ['error', 2],
